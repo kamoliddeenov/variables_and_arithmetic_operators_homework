ab42de2a8964f2acd46db467c11608edc54557e1
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-20 22:53:58 +0500

    2-misol

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,7 +1,4 @@
-# Create a variable "a"  and assign it the value of the int 3.
-
-# Create a variable "b"  and assign it the value of the int 4.
-
-# Create a variable called 'answer'.
-
-# Find the sum of a and b than assign to 'answer'
+a = 3
+b = 4
+answer = a + b
+print(answer)
