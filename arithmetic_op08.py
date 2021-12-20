bd7cb9d515021befad55479081ff378bc5caa52c
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-20 23:06:04 +0500

    8-misol

--- arithmetic_op08.py
+++ arithmetic_op08.py
@@ -1,9 +1,5 @@
-# Create a variable "a"  and assign it the value of the int 3.
-
-# Create a variable "b"  and assign it the value of the int 4.
-
-# Create a variable "c"  and assign it the value of the int 2.
-
-# Create a variable called 'answer'.
-
-# Find the expression value assign to 'answer'. a*b:c
+a = 3
+b = 4
+c = 2
+answer = a * b / c
+print(answer)
