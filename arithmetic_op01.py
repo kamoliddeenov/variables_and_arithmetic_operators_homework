927830adadd217420822e0adb9f8f20a57d100fe
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-20 22:52:28 +0500

    1-misol

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,8 +1,3 @@
-# Create a variable 'num_one'  and assign it the value of the int 5.
-
-# Create a variable 'num_two'   and assign it the value of the int 4.
-
-# Find the sum of  'num_one' and 'num_two' then assign to 'variable_total'
 num_one = 5
 num_two = 4
 variable_total = num_one + num_two
