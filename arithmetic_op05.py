3c6bb1ec5d527f36d64c6bf7692927bb87585337
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-20 23:01:50 +0500

    5-misol

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,7 +1,4 @@
-# Create a variable "a"  and assign it the value of the int 72.
-
-# Create a variable "b"  and assign it the value of the int 9.
-
-# Create a variable called "answer".
-
-# Find the division of a and b then assign to "answer".
+a = 72
+b = 9
+answer = a / b
+print(answer)
