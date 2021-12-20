a8859f5cfedd9b6848b6e19dba01a5907bb37a5b
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-20 22:58:07 +0500

    3-misol

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,5 @@
 # Create a variable "a"  and assign it the value of the int 6.
-
-# Create a variable "b"  and assign it the value of the int 2.
-
-# Create a variable called 'answer'.
-
-# find the difference of a and b then assign to "answer"
+a = 6
+b = 2
+answer = a - b
+print(answer)
