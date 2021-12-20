a70d23449345ae9e24b54f7630cc63b93510184c
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-20 23:07:33 +0500

    9-misol

--- arithmetic_op09.py
+++ arithmetic_op09.py
@@ -1,11 +1,6 @@
-# Create a variable "a"  and assign it the value of the int 8.
-
-# Create a variable "b"  and assign it the value of the int 3.
-
-# Create a variable "c"  and assign it the value of the int 2.
-
-# Create a variable "d"  and assign it the value of the int 4.
-
-# Create a variable called 'answer'.
-
-# Find the expression value assign to 'answer'. a+b - cd .
+a = 8
+b = 3
+c = 2
+d = 4
+answer = a + b - c * d
+print(answer)
