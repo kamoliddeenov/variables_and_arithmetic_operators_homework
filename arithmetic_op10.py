1b76689b089a9954928f2f0aa7fed2bae05c3f24
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-20 23:08:49 +0500

    10-misol

--- arithmetic_op10.py
+++ arithmetic_op10.py
@@ -1,5 +1,3 @@
-# Create a variable "n"  and assign it the value of the 4.
-
-# Create a variable called 'answer'.
-
-# Find the expression value assign to 'answer'. 2^n.
+n = 4
+answer = 2 ** n
+print(answer)
